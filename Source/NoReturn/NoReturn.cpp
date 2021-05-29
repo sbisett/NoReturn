6a4e351566bebda85dbe38d0c24da4ce7cc3c2f9
DESKTOP-GUKQ1BN\Seth <sethbisett96@gmail.com>
2021-05-29 12:09:06 -0500

    Inital Commit

---
+++ Source/NoReturn/NoReturn.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NoReturn.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NoReturn, "NoReturn" );
---
+++ Source/NoReturn/NoReturnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NoReturnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NORETURN_API ANoReturnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
